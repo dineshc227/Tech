85242ab2571e4b9e376e544332e767e89cffa73f
dineshc227 <dk7774496@gmail.com>
2025-09-07 13:58:25 +0530

    add

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scroll for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Enroll alert
+function showEnrollAlert(course) {
+    alert(`You have enrolled in ${course}!`);
+}
